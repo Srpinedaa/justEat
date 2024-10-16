79a87f13613bb9d838b624e4a2957f133e681dea
srpineda31 <sr.pineda31@gmail.com>
2024-10-16 18:59:32 +0200

    primer commit de justEat

---
+++ .history/js/funciones_20241016164848.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    console.log("ready");
+    var checkboxinp = $("#checkboxinp");
+    var domicilio = $("#domicilio");
+    $(".switch").click(function () {
+      // console.log(checkboxinp.checked);
+      // console.log(checkboxinp.data("on"));
+  });
+});
